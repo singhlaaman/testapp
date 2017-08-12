953a5fca11a958306d66dc2b223023f0fbfd271c
singhlaaman <singhlaaman@gmail.com>
2017-08-12 22:18:08 +0100

    All files added

---
+++ search_bar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Search Any Medicine</title>
+		<style type="text/css">
+
+		</style>
+	</head>
+
+<body>
+	<div>
+		<a href="bench-class.php">Click Here for Benchmark</a>
+	</div>
+</body>
+</html>
